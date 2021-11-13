68b75bcfc00c9214569b64f5296719503bbc8cda
dev-ashishsethi <me.ashishsethi@gmail.com>
2021-11-13 17:00:16 +0530

    version 1 done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//https://evilinsult.com/generate_insult.php?lang=en&type=json
+let output=document.getElementById("boxy");
+let btnGenerate=document.querySelector(".btn-generate");
+
+window.addEventListener("load",GetInsulted);
+btnGenerate.addEventListener("click",GetInsulted);
+
+function GetInsulted(){
+    let url=`https://evilinsult.com/generate_insult.php?lang=en&type=json`;
+    fetch(url).then(response => response.json()).then(json => {
+        output.innerText=json.insult}
+        ).catch(errorHandler);
+}
+
+function errorHandler(error){
+    console.log(error);
+}
+
